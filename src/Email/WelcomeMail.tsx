b4dfa36ea308229447651cd4678f9d1227086dd4
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-11-19 14:11:29 +0600

    add mail sender

---
+++ src/Email/WelcomeMail.tsx
@@ -0,0 +1,14 @@
+import { Button, Html } from "@react-email/components";
+
+export default function WelcomeMail() {
+  return (
+    <Html>
+      <Button
+        href="https://example.com"
+        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+      >
+        Click me
+      </Button>
+    </Html>
+  );
+}
