1bc1f60aec64ee50a724392b566135981c87fa3c
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-11-27 18:23:11 +0600

    done yet

--- src/app/preview/page.tsx
+++ src/app/preview/page.tsx
@@ -1,12 +1,7 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-
 export default function PreviewPage() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
   return (
     <>
-      <div>this is new Template : {id}</div>
+      <div>this is new Template </div>
     </>
   );
 }
