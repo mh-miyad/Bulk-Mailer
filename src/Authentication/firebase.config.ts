6c0ed0d3511d89819bc8218581b1c0376f743998
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-11-30 21:04:19 +0600

    again is Change

--- src/Authentication/firebase.config.ts
+++ src/Authentication/firebase.config.ts
@@ -4,8 +4,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
+  apiKey: process.env.NEXT_PUBLIC_FIRE_KEYWORD,
+  authDomain: process.env.NEXT_PUBLIC_GOOGLE_AUTH,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
