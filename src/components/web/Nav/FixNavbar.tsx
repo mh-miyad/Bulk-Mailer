b8dfc383a664b624e197709bdf0ce0f4b352fd34
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-12-14 19:49:22 +0600

    nemi

--- src/components/web/Nav/FixNavbar.tsx
+++ src/components/web/Nav/FixNavbar.tsx
@@ -42,49 +42,49 @@ const FixNavbar = () => {
   }, []);
   return (
     <header
-      className={`fixed  left-0 transition-all ease-linear duration-200 right-0 z-50 max-w-[1440px] mx-auto w-full ${
-        isScrolled ? "top-0 bg-slate-50 dark:bg-[#060d1d] " : "top-0"
+      className={`fixed left-0 right-0 z-50 mx-auto w-full max-w-[1440px] transition-all duration-200 ease-linear ${
+        isScrolled ? "top-0 bg-slate-50 dark:bg-[#060d1d]" : "top-0"
       }`}
     >
       <nav
         className={cn(
-          "w-full transition-all duration-300 border-2 border-transparent px-10",
+          "w-full border-2 border-transparent px-2 transition-all duration-300 2xl:px-10",
           isScrolled
-            ? "py-3  max-w-6xl mx-auto backdrop-blur-lg shadow-sm "
-            : "py-5  border-transparent max-w-full mx-auto "
+            ? "mx-auto max-w-6xl py-3 shadow-sm backdrop-blur-lg"
+            : "mx-auto max-w-full border-transparent py-5",
         )}
       >
         <div className="container mx-auto px-4">
           <div className="flex items-center justify-between">
             {/* logo */}
             <Link href={"/"}>
-              <div className="flex items-center space-x-2 hover:opacity-80 transition-opacity">
+              <div className="flex items-center space-x-2 transition-opacity hover:opacity-80">
                 <CircuitBoard
                   className="h-8 w-8 text-blue-400"
                   strokeWidth={1.5}
                 />
-                <span className="text-xl font-bold bg-gradient-to-r from-primary to-primary/80 bg-clip-text text-transparent uppercase">
+                <span className="bg-gradient-to-r from-primary to-primary/80 bg-clip-text text-xl font-bold uppercase text-transparent">
                   <span className="">Draftex</span>
                 </span>
               </div>
             </Link>
             {/* logo */}
-            <div className="hidden md:flex items-center space-x-8">
+            <div className="hidden items-center space-x-8 md:flex">
               {navLink.map((link, index) => {
                 return (
                   <Link
                     key={index + 1}
                     href={link.href}
                     className={cn(
-                      "relative py-2 px-1 font-medium transition-colors duration-200",
-                      "after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-full",
+                      "relative px-1 py-2 font-medium transition-colors duration-200",
+                      "after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full",
                       "after:origin-left after:scale-x-0 after:bg-primary after:transition-transform",
                       "hover:text-primary hover:after:scale-x-100",
                       `${
                         link.active
                           ? "text-primary after:scale-x-100"
                           : "text-muted-foreground"
-                      }`
+                      }`,
                     )}
                   >
                     {link.label}
@@ -119,7 +119,7 @@ const FixNavbar = () => {
                 </DropdownMenuContent>
               </DropdownMenu>
               <Link href={"/login"}>
-                <Button className="rounded-full uppercase px-6 font-bold">
+                <Button className="rounded-full px-6 font-bold uppercase">
                   Login
                 </Button>
               </Link>
