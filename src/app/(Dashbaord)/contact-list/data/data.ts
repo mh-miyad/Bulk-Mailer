45e0bab4a373f8ccf39c56e7133e4a1d6472c027
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-05-31 18:03:49 +0600

    add conatc list fretures

---
+++ src/app/(Dashbaord)/contact-list/data/data.ts
@@ -0,0 +1,31 @@
+export interface Contact {
+  id: string;
+  name: string;
+  email: string;
+  location: string;
+  dateAdded: string;
+}
+
+export const contacts: Contact[] = [
+  {
+    id: "1",
+    name: "Mh Miyad",
+    email: "miyad@royex.net",
+    location: "Dubai",
+    dateAdded: "2024-06-25",
+  },
+  {
+    id: "2",
+    name: "Sabbir",
+    email: "sabbir@royex.net",
+    location: "Dubai",
+    dateAdded: "2024-06-25",
+  },
+  {
+    id: "3",
+    name: "Rimon",
+    email: "rimon@royex.net",
+    location: "Dubai",
+    dateAdded: "2024-06-25",
+  },
+];
--- src/app/(Dashbaord)/contact-list/page.tsx
+++ src/app/(Dashbaord)/contact-list/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Table,
   TableBody,
@@ -8,87 +20,178 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+import { Trash } from "lucide-react";
+import { useState } from "react";
+import { Contact, contacts } from "./data/data";
+
 const ContactList = () => {
+  const [contact, setContacts] = useState(contacts);
+  const [selectedContacts, setSelectedContacts] = useState<Contact[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newContact, setNewContact] = useState<Contact>({
+    id: "",
+    name: "",
+    email: "",
+    location: "",
+    dateAdded: new Date().toLocaleString(),
+  });
+
+  // Handle header checkbox toggle
+  const handleSelectAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setSelectedContacts(contacts.map((contact) => contact));
+    } else {
+      setSelectedContacts([]);
+    }
+  };
+
+  const handleSelectContact = (contact: Contact) => {
+    setSelectedContacts((prev) =>
+      prev.includes(contact)
+        ? prev.filter((c) => c !== contact)
+        : [...prev, contact],
+    );
+  };
+
+  const handleDeleteContact = (id: string) => {
+    setContacts((prev) => prev.filter((contact) => contact.id !== id));
+  };
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleSaveContact = () => {
+    setContacts((prev) => [...prev, newContact]);
+    setIsModalOpen(false);
+  };
+
   return (
     <>
+      <div className="flex items-center justify-between pb-5">
+        <h1 className="mb-6 text-3xl font-bold">Contact List</h1>
+        <div className="space-x-2">
+          <Button onClick={handleOpenModal}>Add Contact</Button>
+        </div>
+      </div>
       <section>
         <Card className="p-5">
-          <Table>
+          <Table className="w-full border-0">
             <TableHeader>
               <TableRow>
-                <TableHead className="w-[100px]">Invoice</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Method</TableHead>
-                <TableHead className="text-right">Amount</TableHead>
+                <TableHead className="">
+                  <input
+                    type="checkbox"
+                    onChange={handleSelectAll}
+                    checked={selectedContacts.length === contacts.length}
+                  />
+                </TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Email</TableHead>
+                <TableHead>Date Added</TableHead>
+                <TableHead>Location</TableHead>
+                <TableHead>Action</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {invoices.map((invoice) => (
-                <TableRow key={invoice.invoice}>
+              {contact.map((list) => (
+                <TableRow key={list.id}>
                   <TableCell className="font-medium">
-                    {invoice.invoice}
+                    <input
+                      type="checkbox"
+                      checked={selectedContacts.includes(list)}
+                      onChange={() => handleSelectContact(list)}
+                    />
                   </TableCell>
-                  <TableCell>{invoice.paymentStatus}</TableCell>
-                  <TableCell>{invoice.paymentMethod}</TableCell>
-                  <TableCell className="text-right">
-                    {invoice.totalAmount}
+                  <TableCell className="font-medium">{list.name}</TableCell>
+                  <TableCell className="font-medium">{list.email}</TableCell>
+                  <TableCell className="font-medium">
+                    {list.dateAdded}
+                  </TableCell>
+                  <TableCell className="font-medium">{list.location}</TableCell>
+                  <TableCell className="text-right font-medium">
+                    <div className="flex items-center gap-2">
+                      <Button
+                        variant={"destructive"}
+                        onClick={() => handleDeleteContact(list.id)}
+                      >
+                        <Trash />
+                      </Button>
+                    </div>
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
             <TableFooter>
               <TableRow>
-                <TableCell colSpan={3}>Total</TableCell>
-                <TableCell className="text-right">$2,500.00</TableCell>
+                <TableCell colSpan={5}>
+                  <div className="text-right">
+                    <span>Total</span>
+                  </div>
+                </TableCell>
+                <TableCell className="text-right">{contacts.length}</TableCell>
               </TableRow>
             </TableFooter>
           </Table>
         </Card>
       </section>
+      <Dialog open={isModalOpen} onOpenChange={handleCloseModal}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>New Conact </DialogTitle>
+            <DialogDescription>
+              {` Make changes to your profile here. Click save when you're done.`}
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input
+                id="name"
+                className="col-span-3"
+                onChange={(e) =>
+                  setNewContact({ ...newContact, name: e.target.value })
+                }
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="email" className="text-right">
+                Email
+              </Label>
+              <Input
+                id="email"
+                className="col-span-3"
+                onChange={(e) =>
+                  setNewContact({ ...newContact, email: e.target.value })
+                }
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="location" className="text-right">
+                Location
+              </Label>
+              <Input
+                id="location"
+                className="col-span-3"
+                onChange={(e) =>
+                  setNewContact({ ...newContact, location: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit" onClick={handleSaveContact}>
+              Save Contact
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
